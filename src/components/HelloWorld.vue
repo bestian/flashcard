ba2022db2ee735008bd30276a84cdac309bc735c
Bestian Tang <bestian@gmail.com>
2019-06-16 09:22:24 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,127 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="ui centered red card clickable" v-if="!flip" @click="flipCard()">
+      <h1 class="ui header" v-if = "op == '+'">{{ n1 }} + {{ n2 }} = ?</h1>
+      <h1 class="ui header" v-if = "op == '-'">{{ n1 }} - {{ n2 }} = ?</h1>
+      <h1 class="ui header" v-if = "op == '*'">{{ n1 }} × {{ n2 }} = ?</h1>
+    </div>
+
+    <div class="ui centered orange fliped card clickable" v-else @click="flipCard()">
+      <h1 class="ui blue header" v-if = "op == '+'">{{ n1 + n2 }}</h1>
+      <h1 class="ui blue header"  v-if = "op == '-'">{{ n1 - n2 }}</h1>
+      <h1 class="ui blue header"  v-if = "op == '*'">{{ n1 * n2 }}</h1>
+    </div>
+
+
+    <div class="ui form container no-print">
+      <div class="inline fields">
+        <div class="field">
+          <div class="ui labeled input">
+            <div class="ui label">數字下限</div>
+            <input id="r" type="number" v-model="min" step = "5">
+          </div>
+        </div>
+        <div class="field">
+          <div class="ui labeled input">
+            <div class="ui label">數字上限</div>
+            <input id="r" type="number" v-model="range" step = "5" autofocus="">
+          </div>
+        </div>
+      </div>
+      <div class="inline fields">
+        <label>選擇算法：</label>
+        <div class="field" v-for="o in ops" v-bind:key="o.v">
+          <div class="ui radio checkbox">
+            <input type="radio" name="year" v-bind:value="o.v" v-model = "op">
+            <label class="clickable" @click = "op = o.v">{{o.t}}</label>
+          </div>
+        </div>
+      </div>
+    </div>
+     
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      flip: false,
+      op: '+',
+      ops: [
+        {v: '+', t: '+'},
+        {v: '-', t: '-'},
+        {v: '*', t: '×'}
+      ],
+      n1: 0,
+      n2: 0,
+      min: 0,
+      range: 10
+    }
+  },
+  methods: {
+    flipCard: function () {
+      if (this.flip) {
+        this.reset()
+        this.flip = false
+      } else {
+        this.flip = true
+      }
+    },
+    reset: function () {
+      this.n1 = Math.floor(Math.random() * (this.range - this.min)) + Number(this.min)
+      this.n2 = Math.floor(Math.random() * (this.range - this.min)) + Number(this.min)
+    }
+  },
+  mounted () {
+    this.reset()
+    window.addEventListener('keydown', this.flipCard)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1, h2 {
+  font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+  color: #35495E;
 }
+
+h1 {
+  font-size: 4em !important;
+  margin-left: -.3em !important;
+}
+
+.card {
+  height: 50vh;
+  width: auto;
+  justify-content: center;
+}
+
+.card.orange {
+  background-color: #fc9 !important;
+}
+
+.card.red {
+  background-color: #fcc !important;
+}
+
+.card.fliped .header {
+  font-size: 10em !important;
+}
+
 </style>
